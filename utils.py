a0f331d35f19782ffaec03854f0fa6f424edd9a5
anaramirli <anar.amirli@gmail.com>
2018-11-20 23:57:53 +0400

    updates in document

---
+++ utils.py
@@ -0,0 +1,74 @@
+# Copyright 2018 Anar Amirli
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+def plot_confusion_matrix(cm,
+                          classes,
+                          xlabel,
+                          ylabel,
+                          normalize=False,
+                          cmap=plt.cm.Blues):
+    """
+    Plot the given confusion matrix cm as a matrix using and return the
+    resulting axes object containing the plot.
+    Parameters
+    ----------
+    cm : ndarray
+        Confusion matrix as a 2D numpy.array.
+    classes : list of str
+        Names of classified classes.
+    xlabel : str
+    Label of the horizontal axis.
+    ylabel : str
+    Label of the vertical axis.
+    normalize : bool
+        If True, the confusion matrix will be normalized. Otherwise, the values
+        in the given confusion matrix will be plotted as they are.
+    cmap : matplotlib.colormap
+        Colormap to use when plotting the confusion matrix.
+    Returns
+    -------
+    fig : matplotlib.figure
+        Plot figure.
+    ax : matplotlib.Axes
+        matplotlib.Axes object with horizontal bar chart plotted.
+    References
+    ----------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    vmin = None
+    vmax = None
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        vmin = 0
+        vmax = 1
+
+    plt.figure(figsize=(4, 4))
+    cax = plt.imshow(
+        cm, interpolation='nearest', vmin=vmin, vmax=vmax, cmap=cmap)
+    plt.colorbar(cax)
+    
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+
+
+    plt.yticks(tick_marks,classes)
+
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        cell_str = '{:.2f}'.format(cm[i, j]) if normalize else str(cm[i, j])
+        plt.text(
+            j,
+            i,
+            cell_str,
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+  
+    plt.show()
